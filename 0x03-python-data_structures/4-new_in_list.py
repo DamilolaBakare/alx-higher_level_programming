aadcfcdb76927913cb696ec9835a2b71899f7910
Damilola Bakare <adamilola414@gmail.com>
2022-06-04 03:35:09 -0700

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,10 +3,10 @@ def new_in_list(my_list, idx, element):
     cp = []
     for i in range(len(my_list)):
         cp.append(my_list[i])
-        if idx < 0:
+    if idx < 0:
             return cp
-        elif idx >= len(my_list):
+    elif idx >= len(my_list):
             return cp
-        else:
+    else:
             cp[idx] = element
             return cp
